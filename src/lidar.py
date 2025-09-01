6d4c007550589d91fe6c812c97ebdb951b5f3d60
Rainerino <albertyanyy@gmail.com>
2025-09-01 16:50:22 +0800

    wip

--- src/lidar.py
+++ src/lidar.py
@@ -11,21 +11,30 @@ class LidarSensor:
     ray_phi: np.ndarray = attr.field()
     ray_theta: np.ndarray = attr.field()
     site_id: int = attr.field(init=False)
-    _parent_id: int = attr.field(init=False)
+    __parent_id: int = attr.field(init=False)
     cutoff_dist: float = attr.field(default=100.0)
     pcl_frame: np.ndarray = attr.field(init=False)
-
-
+    
+    # For Optimization
+    __nray: int = attr.field(init=False)
+    __dist: np.ndarray = attr.field(init=False)
+    __geomid: np.ndarray = attr.field(init=False)
+    
     def __attrs_post_init__(self) -> None:
+        self.__nray = self.ray_phi.shape[0]
         # Find site in model. If not found, raise error
         try:
             self.site_id = self.model.site(self.site_name).id
-            self._parent_id = self.model.site(self.site_id).bodyid
+            self.__parent_id = self.model.site(self.site_id).bodyid
         except ValueError as e:
             raise ValueError(f"Site '{self.site_name}' not found in model") from e
 
         if self.ray_phi.shape[0] != self.ray_theta.shape[0]:
             raise ValueError("ray_phi and ray_theta must have the same shape")
+        
+        # Initialize 
+        self.__dist = np.full(self.__nray, self.cutoff_dist, dtype=np.float64)
+        self.__geomid = np.full(self.__nray, 0, dtype=np.int32)
 
     def update(self) -> None:
         # Uniformly generate vec from site's pose and lidar settings
@@ -33,7 +42,6 @@ class LidarSensor:
         site_pos = self.data.site_xpos[self.site_id]
         pnt = np.array([site_pos]).T
         site_mat = self.data.site_xmat[self.site_id].reshape(3, 3)
-        print(site_mat)
         x = np.cos(self.ray_phi) * np.cos(self.ray_theta)
         y = np.cos(self.ray_phi) * np.sin(self.ray_theta)
         z = np.sin(self.ray_phi)
@@ -41,11 +49,7 @@ class LidarSensor:
         world_vecs = (site_mat @ local_vecs.T).T
         world_vecs /=np.linalg.norm(world_vecs, axis=1, keepdims=True)
         world_vecs_flat = world_vecs.flatten()
-        nray = self.ray_phi.shape[0]
-        geomid = np.zeros(nray, dtype=np.int32)
-        # Data stored in dist
-        dist = np.full(nray, self.cutoff_dist, dtype=np.float64)
-
+    
         # Get the ray casting results
         mujoco.mj_multiRay(
             m=self.model,
@@ -55,17 +59,16 @@ class LidarSensor:
             geomgroup=None,
             flg_static=1,
             bodyexclude=-1,
-            geomid=geomid,
-            dist=dist,
-            nray=nray,
+            geomid=self.__geomid,
+            dist=self.__dist,
+            nray=self.__nray,
             cutoff=self.cutoff_dist
         )
         # Calculate the point's position in local frame from vec + dist
-        dist[geomid == -1] = self.cutoff_dist
+        self.__dist[self.__geomid == -1] = self.cutoff_dist
 
-        # 4. Update the pcl frame with local frame data
-        # Point position = direction_vector * distance
-        self.pcl_frame = local_vecs * dist[:, np.newaxis]
+        # Update the pcl frame with local frame data
+        self.pcl_frame = local_vecs * self.__dist[:, np.newaxis]
 
     def get_data_in_local_frame(self) -> np.ndarray:
         return self.pcl_frame
--- test/test_scene1.py
+++ test/test_scene1.py
@@ -14,44 +14,52 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 from src.lidar import LidarSensor
 
-ray_theta, ray_phi = generate_grid_scan_pattern(
-    num_ray_cols=50,
-    num_ray_rows=50,
-)
+# ray_theta, ray_phi = generate_grid_scan_pattern(
+#     num_ray_cols=100,
+#     num_ray_rows=100,
+# )
 # ray_theta, ray_phi = generate_HDL64()
-# ray_theta, ray_phi = LivoxGenerator("mid360").sample_ray_angles()
+ray_theta, ray_phi = LivoxGenerator("mid360").sample_ray_angles()
 
 RANDOM_RANGE = 5
 CUTOFF = 5
+VISUALIZE = False
 
 def test_scene1():
     # Get the path to the scene1.xml file
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    filename = os.path.join(current_dir, "scene1.xml")
+    filename = os.path.join(current_dir, "scene2.xml")
     try:
         with open(filename, "r") as f:
             original_xml = f.read()
     except FileNotFoundError:
         print(f"Error: The file '{filename}' was not found.")
         exit()
-    boxes_xml = ""
-    
-    for i in range(ray_theta.shape[0]):
-        # Unique position and color for each box
-        start_pos = f"{np.random.uniform(-RANDOM_RANGE, RANDOM_RANGE)} {np.random.uniform(-RANDOM_RANGE, RANDOM_RANGE)} {np.random.uniform(0, RANDOM_RANGE/2)}"
-        color = f"{np.random.uniform(0, 1)} {np.random.uniform(0, 1)} {np.random.uniform(0, 1)} 1"
-        boxes_xml += f"""
-            <body name="point_{i}" mocap="true" pos="{start_pos}">
-                <site name="site_{i}" type="sphere" size="0.02" rgba="{color}"/>
-            </body>
-          """
-    if "<worldbody>" in original_xml:
-        full_xml = original_xml.replace("<worldbody>", f"<worldbody>{boxes_xml}", 1)
-    else:
-        print(f"Error: Could not find <worldbody> tag in {filename}.")
-        exit()
+        
+    if VISUALIZE:
+        boxes_xml = ""
+        
+        for i in range(ray_theta.shape[0]):
+            # Unique position and color for each box
+            start_pos = f"{np.random.uniform(-RANDOM_RANGE, RANDOM_RANGE)} {np.random.uniform(-RANDOM_RANGE, RANDOM_RANGE)} {np.random.uniform(0, RANDOM_RANGE/2)}"
+            color = f"{np.random.uniform(0, 1)} {np.random.uniform(0, 1)} {np.random.uniform(0, 1)} 1"
+            boxes_xml += f"""
+                <body name="point_{i}" mocap="true" pos="{start_pos}">
+                    <site name="site_{i}" type="sphere" size="0.02" rgba="{color}"/>
+                </body>
+            """
+        if "<worldbody>" in original_xml:
+            full_xml = original_xml.replace("<worldbody>", f"<worldbody>{boxes_xml}", 1)
+            # Initalize M x N number of boxes, and visualize them.
+            mocap_ids = [model.body(f"point_{i}").mocapid[0] for i in range(ray_theta.shape[0])]
+        else:
+            print(f"Error: Could not find <worldbody> tag in {filename}.")
+            exit()
     # Load the MuJoCo model
-    model = mujoco.MjModel.from_xml_string(full_xml)
+    if VISUALIZE:
+        model = mujoco.MjModel.from_xml_string(full_xml)
+    else:
+        model = mujoco.MjModel.from_xml_path(filename)
     data = mujoco.MjData(model)
     
     # Initialize lidar sensor
@@ -64,8 +72,6 @@ def test_scene1():
         cutoff_dist=CUTOFF,
     )
 
-    # Initalize M x N number of boxes, and visualize them.
-    mocap_ids = [model.body(f"point_{i}").mocapid[0] for i in range(ray_theta.shape[0])]
     # Create viewer for visualization
     with mujoco.viewer.launch_passive(model, data) as viewer:
         # Initialize timing variables
@@ -86,9 +92,10 @@ def test_scene1():
 
             # Get data in local frame
             pcl: np.ndarray = lidar_sensor.get_data_in_world_frame()
-            for i, pt in enumerate(pcl):
-                mocap_id = mocap_ids[i]
-                data.mocap_pos[mocap_id] = pt
+            if VISUALIZE:
+                for i, pt in enumerate(pcl):
+                    mocap_id = mocap_ids[i]
+                    data.mocap_pos[mocap_id] = pt
             # TODO: how to visualize the point cloud?
 
             # Synchronize viewer
